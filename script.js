b7ea417edac0f6ddadd72c819a6fb3f442671d27
Lakshit taygi <tyagilakshit6@gmail.com>
2025-11-03 15:48:18 +0530

    music.svg added and some js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Lets write javaScript");
 
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/songs/"); //   fecthing the songs from local server
-
+ 
   let response = await a.text();
   // console.log(response)
   let div = document.createElement("div");
@@ -13,20 +13,35 @@ async function getSongs() {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
       // Parse the HTML and extract links ending with .mp3
-      songs.push(element.href);
+      songs.push(element.href.split("/songs/")[1]);
     }
   }
   return songs; // Return the array of songs
 }
 
-async function main() {
-  let songs = await getSongs();
-  console.log(songs);
+async function main() { 
+  let songs = await getSongs();   
+  console.log(songs);   
+  let index = 0;
+  let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+  for(const song of songs){ //we use for of loop because its a array
+     songUL.innerHTML = songUL.innerHTML + `<li> ${index+1} ${song.replaceAll("%20"," ")} </li>` ;
+     index++;
+  }
+  //   play the first song. 
 
-  //   play the first song
+  var audio = new Audio(songs[1]);   
+  audio.play();        
 
-  var audio = new Audio(songs[0]);
-  audio.play();
+  audio.addEventListener("loadeddata" , () => {
+
+    let duration = audio.duration ;
+    console.log(duration)  //the duration variable now holds the duration (in seconds) of the audio clip
+  }); 
 }
-
-main();
+     
+main();          
+ 
+              
+ 
+  
