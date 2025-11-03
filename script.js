1dcdb2b1a3fdf3d10dd668751bbfe309ea538a15
Lakshit taygi <tyagilakshit6@gmail.com>
2025-11-03 15:18:19 +0530

    music.svg added

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 console.log("Lets write javaScript");
 
-
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:3000/songs/")
+  let a = await fetch("http://127.0.0.1:3000/songs/"); //   fecthing the songs from local server
 
-    let response = await a.text();
-    // console.log(response)
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    let songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
-        }
+  let response = await a.text();
+  // console.log(response)
+  let div = document.createElement("div");
+  div.innerHTML = response; //creating demo div container to store songs cause before this its just a string not comes in html that we need to parse
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      // Parse the HTML and extract links ending with .mp3
+      songs.push(element.href);
     }
-    return songs
+  }
+  return songs; // Return the array of songs
 }
 
 async function main() {
-    // Get the list of all the songs
-    let songs = await getSongs()
-    console.log(songs)
+  let songs = await getSongs();
+  console.log(songs);
 
-    // Play the first song
-    var audio = new Audio(songs[0]);
-    audio.play();
+  //   play the first song
+
+  var audio = new Audio(songs[0]);
+  audio.play();
 }
 
-main()
+main();
